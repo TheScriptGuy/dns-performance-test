c90a2cdacdbcee5fcbb0920c5a8097ae467ccaf1
Git <gituser@calvin.home.remotenode.org>
2021-05-22 14:08:30 -0700

    Adding tag feature

--- systemInfo/systemInfo.py
+++ systemInfo/systemInfo.py
@@ -6,6 +6,7 @@ from os import path
 class systemInfo:
 
     uuidFilename = 'uuid.cfg'
+    tagFilename = 'tag.cfg'
 
     def generateUuid(self):
         return str(uuid.uuid4())
@@ -18,14 +19,24 @@ class systemInfo:
             f_uuid.close()
 
 
-    def getUuid (self):
+    def getUuid(self):
         self.createUuidIfNotExist()
         f_uuid = open(self.uuidFilename,'r')
         myUuid = f_uuid.readline()
         f_uuid.close()
         return myUuid
 
+    def getTag(self):
+        if path.exists(self.tagFilename):
+            f_tag = open(self.tagFilename,'r')
+            myTag = f_tag.readline().rstrip()
+            f_tag.close()
+            return myTag
+        else:
+            return ""
+
     def __init__(self):
         self.hostname = socket.gethostname()
         self.uuid = self.getUuid()
+        self.deviceTag = self.getTag()
     
